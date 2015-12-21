0d46a5ffdd4e0b29d74abde71746d12d665708f8
Nathan O <nro5@pitt.edu>
2015-12-21 15:55:58 -0500

    Added a TODO comment to the batch replacement code

--- src/main/java/DiversityOperator.java
+++ src/main/java/DiversityOperator.java
@@ -52,6 +52,7 @@ public class DiversityOperator implements Serializable {
 
     /**
      * TODO: ADD SMARTER PROBABILITIES
+     * TODO: SORT BUFFER
      * This function is only called when the buffers get full so no need to include that check within the function.
      * Remember to empty the buffers before returning.
      */
